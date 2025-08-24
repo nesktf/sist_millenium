f0c22199e2850d8becd2627d747daaeb3c547dd3
nesktf <nesktf@proton.me>
2025-08-24 20:48:28 -0300

    TODOs

--- src/app/api/v1/prod/route.tsx
+++ src/app/api/v1/prod/route.tsx
@@ -2,20 +2,27 @@ let prods = [
   {name: "test", desc: "Funny product", price: 1000000000000},
 ];
 
+// TODO:
+// 1. Registrar producto -> Registrar un nuevo producto. POST para productos
+// 2. Actualizar producto -> Modificar productos existentes. PUT para productos
+// 3. Gestionar depósitos -> Registrar y modificar datos de los depósitos. POST & PUT para depósitos
+// 4. Generar requerimientos de reposición -> Generar req. rep automáticamente. Ejecutar registor en la DB cuando stock llege a un número límite.
+// 5. Gestionar ingreso de stock -> Actualizar stocks, incrementar numerito
+// 6. Gestionar egreso de stock -> Actualizar stocks, decrementar numerito
+// 7. Transferir stock entre depósitos -> Actualizar stocks, incrementar numerito en un lado decrementar en otro
+// 8. Consultar disponibilidad de stock -> GET para los stocks
+// 9. Generar requerimientos de reposición -> Duplicado del 4. Ignorar
+
 // Endpoints
-// Consultar productos
 export async function GET(req: Request) {
   return Response.json(prods[0]);
 }
-// Registrar producto
 export async function POST(req: Request) {
   return Response.error();
 }
-// Modificar producto
 export async function PUT(req: Request) {
   return Response.error();
 }
-// Eliminar producto
 export async function DELETE(req: Request) {
   return Response.error();
 }
