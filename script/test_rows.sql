4f9d20e4cc0072544165fc0c01abc95e7d33c4d0
nesktf <nesktf@proton.me>
2025-09-01 20:23:30 -0300

    Update rows script

--- script/test_rows.sql
+++ script/test_rows.sql
@@ -1,5 +1,5 @@
 INSERT INTO "Articulo" (codigo, nombre, id_categoria, id_marca, u_medida)
 VALUES
-  ('abc123', 'GTX 2080TI', NULL, NULL, 'm'),
-  ('abc456', 'GTX 3080TI', NULL, NULL, 'm'),
-  ('abc789', 'GTX 4080TI', NULL, NULL, 'm');
+  ('abc123', 'GTX 2080TI', NULL, NULL),
+  ('abc456', 'GTX 3080TI', NULL, NULL),
+  ('abc789', 'GTX 4080TI', NULL, NULL);
