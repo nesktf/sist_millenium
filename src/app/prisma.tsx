a4375851b48d039f06622701c52c70f3914234b7
nesktf <nesktf@proton.me>
2025-09-02 12:20:52 -0300

    Untested movement schema things

--- src/app/prisma.tsx
+++ src/app/prisma.tsx
@@ -1,3 +1,4 @@
+import assert from "assert";
 import { PrismaClient, TipoMovimiento } from "../generated/prisma";
 
 const prisma = new PrismaClient();
@@ -133,22 +134,22 @@ export class ArticuloDepositoData {
 };
 
 export class MovimientoStockData {
-  private id_src: DBId;
+  private id_dst: DBId;
   private tipo: TipoMovimiento;
   private articulos: Array<ArticuloDepositoData>;
-  private id_dst?: DBId;
+  private id_src?: DBId;
   private comprobante?: string;
 
-  private constructor(id_src: DBId, tipo: TipoMovimiento, articulos: Array<ArticuloDepositoData>,
-                      id_dst?: DBId | null, comprobante?: string | null) {
+  private constructor(id_dst: DBId, tipo: TipoMovimiento, articulos: Array<ArticuloDepositoData>,
+                      id_src?: DBId | null, comprobante?: string | null) {
     if (articulos.length == 0) {
       throw new Error("Invalid array size");
     }
-    this.id_src = id_src;
+      this.id_dst = id_dst;
     this.tipo = tipo;
     this.articulos = articulos;
-    if (id_dst) {
-      this.id_dst = id_dst;
+    if (id_src) {
+      this.id_src = id_src;
     }
     if (comprobante) {
       this.comprobante = comprobante;
@@ -168,65 +169,64 @@ export class MovimientoStockData {
     return new MovimientoStockData(from, TipoMovimiento.TRANSFERENCIA, articulos, to, null);
   }
 
-  getDeposito(): DBId { return this.id_src; }
+  getDeposito(): DBId { return this.id_dst; }
   getTipo(): TipoMovimiento { return this.tipo; }
   getArticulos(): Array<ArticuloDepositoData> { return this.articulos; }
   hasComprobante(): boolean { return this.hasComprobante != null; }
   getComprobante(): string | null { return this.comprobante ? this.comprobante : null; }
-  getDestino(): DBId | null { return this.id_dst ? this.id_dst : null; }
+  getFuente(): DBId | null { return this.id_src? this.id_src: null; }
 };
-
-async function findOrLoadArticDepos(deposito: DBId,
-                                    articulos: Array<ArticuloDepositoData>): Promise<Array<DBId>> {
-  return await Promise.all(articulos.map(async (articulo) => {
-    let artic_depos_entry = await prisma.articDepos.findFirst({
-      where: {
-        id_deposito: deposito,
-        id_articulo: articulo.getId(),
-      }
-    });
-    if (artic_depos_entry) {
-      return artic_depos_entry.id;
-    } else {
-      return await prisma.articDepos.create({
-        data: {
-          id_deposito: deposito,
-          id_articulo: articulo.getId(),
-          stock: articulo.getStock(),
-          stock_min: 10000, // FIXME: Where do i get this from?
-        }
-      })
-      .then((entry): DBId => {
-        return entry.id;
-      })
-    }
-  }));
-}
 
 type RegistroMov = {
   src: { id: DBId, date: Date, } | null,
   dst: { id: DBId, date: Date, }
 }
 
-export async function registerMovimiento(movimiento: MovimientoStockData): Promise<RegistroMov> {
-  let tipo = movimiento.getTipo();
-  let deposito = movimiento.getDeposito();
-  let articulos = movimiento.getArticulos();
-
-  let dst_entry = await prisma.movimientoStock.create({
-    data: {
-      fecha_hora: new Date(),
-      tipo: tipo,
+// Update or delete existing artic_depos entry
+async function updateArticDeposStock(artic: {id: DBId, stock: number},
+                                     deposito: DBId): Promise<DBId | null> {
+  let entry = await prisma.articDepos.findFirstOrThrow({
+    where: {
       id_deposito: deposito,
-      num_comprobante: movimiento.getComprobante(),
+      id_articulo: artic.id,
     }
   });
-  var src_entry: { id: DBId, date: Date } | null = null;
 
-  if (tipo == TipoMovimiento.INGRESO) {
-    let detalles = await findOrLoadArticDepos(deposito, articulos)
+  let new_stock = artic.stock + artic.stock;
+  if (new_stock < 0) {
+    throw new Error("Invalid stock");
+  }
+
+  if (new_stock == 0) {
+    // Delete entry
+    await prisma.articDepos.delete({
+      where: { id: artic.id }
+    });
+    return null
+  }
+
+  // Update stock
+  await prisma.articDepos.update({
+    where: { id: artic.id },
+    data: { stock: new_stock },
+  })
+  return entry.id;
+}
+
+export async function registerMovimiento(movimiento: MovimientoStockData): Promise<RegistroMov> {
+  let tipo = movimiento.getTipo();
+  let articulos = movimiento.getArticulos();
+
+  const add_artic_depos = async (deposito: DBId) => {
+    return await Promise.all(articulos.map(async (articulo) => {
+      assert(articulo.getStock() > 0, "Stock inválido");
+      let data = {id: articulo.getId(), stock: articulo.getStock()};
+      return await updateArticDeposStock(data, deposito);
+    }))
     .then((articulos_depos) => {
-      return articulos_depos.map((id_artic_depos, idx: number) => {
+      return articulos_depos.map((id_artic_depos: number | null, idx: number) => {
+        // Solo pueden haberse actualizado o creado entradas
+        assert(id_artic_depos != null, "Ingreso no puede eliminar articulos_depos");
         return {
           id_movimiento: dst_entry.id,
           id_artic_depos: id_artic_depos,
@@ -234,16 +234,88 @@ export async function registerMovimiento(movimiento: MovimientoStockData): Promi
         }
       });
     });
+  };
+  const subs_artic_depos = async (deposito: DBId) => {
+    let detalles = await Promise.all(articulos.map(async (articulo) => {
+      assert(articulo.getStock() < 0, "Stock inválido");
+      let data = {id: articulo.getId(), stock: articulo.getStock()};
+      return await updateArticDeposStock(data, deposito);
+    }))
+    .then((articulos_depos) => {
+      return articulos_depos.map((id_artic_depos: number | null, idx: number) => {
+        if (id_artic_depos == null) {
+          return null;
+        }
+        // FIXME: Qué detalle crear cuando un artículo en depósito es eliminado?
+        return {
+          id_movimiento: dst_entry.id,
+          id_artic_depos: id_artic_depos,
+          cantidad: articulos[idx].getStock(),
+        }
+      });
+    });
+    return detalles.filter((detalle) => {
+      return detalle != null;
+    });
+  };
 
+  // 1. Crear entrada de movimiento de depósito
+  let dst_deposito = movimiento.getDeposito();
+  let date = new Date();
+  let dst_entry = await prisma.movimientoStock.create({
+    data: {
+      fecha_hora: date,
+      tipo: tipo,
+      id_deposito: dst_deposito,
+      num_comprobante: movimiento.getComprobante(),
+    }
+  });
+  var src_entry_out: { id: DBId, date: Date } | null = null;
+  if (tipo == TipoMovimiento.INGRESO) {
+    // 2. Crear o actualizar artículos en depósitos con más stock
+    let detalles = await add_artic_depos(dst_deposito);
+
+    // 3. Crear detalles de movimientos con los artículos de depósito modificados/creados
     await Promise.all(detalles.map(async (detalle) => {
       await prisma.detalleMovimiento.create({data: detalle});
     }));
   } else if (tipo == TipoMovimiento.EGRESO) {
+    // 2. Eliminar o actualizar artículos en depósitos con menos stock
+    let detalles = await subs_artic_depos(dst_deposito);
 
+    // 3. Crear detalles de movimientos con los artículos de depósito modificados
+    await Promise.all(detalles.map(async (detalle) => {
+      await prisma.detalleMovimiento.create({data: detalle});
+    }));
   } else if (tipo == TipoMovimiento.TRANSFERENCIA) {
+    // 2. Crear otro movimiento para el depósito fuente
+    let src_deposito = movimiento.getFuente();
+    assert(src_deposito != null);
+    let src_entry = await prisma.movimientoStock.create({
+      data: {
+        fecha_hora: date,
+        tipo: tipo,
+        id_deposito: src_deposito,
+        num_comprobante: movimiento.getComprobante(),
+      }
+    })
+    src_entry_out = { id: src_entry.id, date: date};
 
+    // 3. Eliminar o actualizar artículos en el depósito fuente
+    let src_detalles = await subs_artic_depos(src_deposito);
+
+    // 4. crear o actualizar artículos en el depósito destino
+    let dst_detalles = await add_artic_depos(dst_deposito);
+
+    // 5. Cargar todos los detalles
+    await Promise.all(src_detalles.map(async (detalle) => {
+      await prisma.detalleMovimiento.create({data: detalle});
+    }));
+    await Promise.all(dst_detalles.map(async (detalle) => {
+      await prisma.detalleMovimiento.create({data: detalle});
+    }));
   }
 
-  return { src: src_entry, dst: { id: dst_entry.id, date: dst_entry.fecha_hora } };
+  return { src: src_entry_out, dst: { id: dst_entry.id, date: dst_entry.fecha_hora } };
 }
 
