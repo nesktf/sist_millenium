e8437fe8b7db5cffdaa31aaed292108b147cb784
nesktf <nesktf@proton.me>
2025-09-18 12:20:38 -0300

    Initial mockup for loading lists

---
+++ src/app/proveedor/comprobante/page.tsx
@@ -0,0 +1,142 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+
+export class ProvComprTupleElem {
+  id: number;
+  articulo: string;
+  cantidad: number;
+  precio: number;
+  observacion?: string;
+
+  constructor(id: number, articulo: string, cantidad: number,
+              precio: number, observacion?: string) {
+    this.id = id;
+    this.articulo = articulo;
+    this.cantidad = cantidad;
+    this.precio = precio;
+    this.observacion = observacion;
+  }
+};
+
+function ProvComprTable(input: {elems: Array<ProvComprTupleElem>}) {
+  return (
+    <div className="overflow-x-auto">
+      <table className="table table-zebra w-full text-base-content">
+        <thead className="bg-base-200">
+          <tr>
+            <th>Articulo</th>
+            <th>Observación</th>
+            <th className="text-right">Cantidad</th>
+            <th className="text-right">Precio Unitario</th>
+          </tr>
+        </thead>
+        <tbody>
+          {input.elems.map((elem) => (
+            <tr key={elem.id}>
+              <td>{elem.articulo}</td>
+              <td>{elem?.observacion || "-"}</td>
+              <td className="text-right">{elem.cantidad}</td>
+              <td className="text-right">${elem.precio.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+function FormHeader() {
+  const [proveedores, setProveedores ] = useState<any[]>([
+    { id: 1, nombre: "test"}
+  ]);
+  return (
+    <form className="space-y-4 min-w-[32rem]">
+      <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
+        {/* Num comprobante */}
+        <div className="flex items-center gap-2 md:col-span-6">
+          <label className="label">
+            <span className="label-text font-medium mb-2">Número</span>
+          </label>
+          <input
+            type="text"
+            className="input input-bordered w-half"
+            name="numero"
+          />
+        </div>
+
+        {/* Fecha */}
+        <div className="flex items-center gap-2 md:col-span-6">
+          <label className="label">
+            <span className="label-text font-medium mb-2">Fecha</span>
+          </label>
+
+          <input
+            type="date"
+            className="input input-bordered w-half"
+            name="fecha"
+          />
+        </div>
+
+        {/* Letra */}
+        <div className="flex items-center gap-2 md:col-span-6">
+          <label className="label">
+            <span className="label-text font-medium mb-2">Letra</span>
+          </label>
+          <select
+            className="select select-bordered">
+            <option key="A">A</option>
+            <option key="B">B</option>
+            <option key="C">C</option>
+          </select>
+        </div>
+
+        {/* Proveedor*/} 
+        <div className="flex items-center gap-2 md:col-span-6">
+          <label className="label">
+            <span className="label-text font-medium mb-2">Proveedor</span>
+          </label>
+          <select
+            className="select select-bordered">
+            <option value="none" disabled>Seleccionar proveedor</option>
+            {proveedores.map((prov) => (
+              <option key={prov.id} value={prov.id}>{prov.nombre}</option>
+            ))}
+          </select>
+        </div>
+
+      </div>
+    </form>
+  )
+}
+
+export default function ComprProveedorPage() {
+  let things = [new ProvComprTupleElem(1, "thing", 2, 3)]
+  return (
+    <div>
+      <main style={{ padding: "2rem" }}>
+        <div className="mb-4 flex flex-wrap gap-3">
+          <Link href="/proveedor" className="btn btn-outline btn-sm">
+            Volver a proveedores
+          </Link>
+        </div>
+
+        <h1 style={{
+          fontSize: "1.5rem",
+          fontWeight: "bold",
+          marginBottom: "1rem",
+        }}>Cargar comprobante de proveedor</h1>
+
+        <div id="header" className="mb-4 gap-3">
+          {FormHeader()}
+        </div>
+
+        <ProvComprTable
+          elems={things}
+          />
+
+      </main>
+    </div>
+  )
+}
