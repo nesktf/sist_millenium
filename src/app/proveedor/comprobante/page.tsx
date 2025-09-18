47a3a40dfe066d4082d8960428ffcf6607b23826
nesktf <nesktf@proton.me>
2025-09-18 12:36:45 -0300

    Input mockup

--- src/app/proveedor/comprobante/page.tsx
+++ src/app/proveedor/comprobante/page.tsx
@@ -47,68 +47,138 @@ function ProvComprTable(input: {elems: Array<ProvComprTupleElem>}) {
   )
 }
 
+
 function FormHeader() {
   const [proveedores, setProveedores ] = useState<any[]>([
     { id: 1, nombre: "test"}
   ]);
   return (
-    <form className="space-y-4 min-w-[32rem]">
-      <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
-        {/* Num comprobante */}
-        <div className="flex items-center gap-2 md:col-span-6">
-          <label className="label">
-            <span className="label-text font-medium mb-2">Número</span>
-          </label>
-          <input
-            type="text"
-            className="input input-bordered w-half"
-            name="numero"
-          />
+    <div>
+      <h1>input header</h1>
+        <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
+          {/* Num comprobante */}
+          <div className="flex items-center gap-2 md:col-span-6">
+            <label className="label">
+              <span className="label-text font-medium mb-2">Número</span>
+            </label>
+            <input
+              type="text"
+              className="input input-bordered w-half"
+              name="numero"
+            />
+          </div>
+
+          {/* Fecha */}
+          <div className="flex items-center gap-2 md:col-span-6">
+            <label className="label">
+              <span className="label-text font-medium mb-2">Fecha</span>
+            </label>
+
+            <input
+              type="date"
+              className="input input-bordered w-half"
+              name="fecha"
+            />
+          </div>
+
+          {/* Letra */}
+          <div className="flex items-center gap-2 md:col-span-6">
+            <label className="label">
+              <span className="label-text font-medium mb-2">Letra</span>
+            </label>
+            <select
+              className="select select-bordered">
+              <option key="A">A</option>
+              <option key="B">B</option>
+              <option key="C">C</option>
+            </select>
+          </div>
+
+          {/* Proveedor*/} 
+          <div className="flex items-center gap-2 md:col-span-6">
+            <label className="label">
+              <span className="label-text font-medium mb-2">Proveedor</span>
+            </label>
+            <select
+              className="select select-bordered">
+              <option value="none" disabled>Seleccionar proveedor</option>
+              {proveedores.map((prov) => (
+                <option key={prov.id} value={prov.id}>{prov.nombre}</option>
+              ))}
+            </select>
+          </div>
+
         </div>
-
-        {/* Fecha */}
-        <div className="flex items-center gap-2 md:col-span-6">
-          <label className="label">
-            <span className="label-text font-medium mb-2">Fecha</span>
-          </label>
-
-          <input
-            type="date"
-            className="input input-bordered w-half"
-            name="fecha"
-          />
-        </div>
-
-        {/* Letra */}
-        <div className="flex items-center gap-2 md:col-span-6">
-          <label className="label">
-            <span className="label-text font-medium mb-2">Letra</span>
-          </label>
-          <select
-            className="select select-bordered">
-            <option key="A">A</option>
-            <option key="B">B</option>
-            <option key="C">C</option>
-          </select>
-        </div>
-
-        {/* Proveedor*/} 
-        <div className="flex items-center gap-2 md:col-span-6">
-          <label className="label">
-            <span className="label-text font-medium mb-2">Proveedor</span>
-          </label>
-          <select
-            className="select select-bordered">
-            <option value="none" disabled>Seleccionar proveedor</option>
-            {proveedores.map((prov) => (
-              <option key={prov.id} value={prov.id}>{prov.nombre}</option>
-            ))}
-          </select>
-        </div>
-
-      </div>
-    </form>
+    </div>
   )
+}
+
+function FormInput() {
+  const [articulos, setArticulos] = useState<any[]>([
+    { id: 1, nombre: "test-1", },
+    { id: 2, nombre: "test-2", },
+  ]);
+  return (
+    <div>
+      <h1>input data</h1>
+        <div className="grid grid-cols-1 md:grid-cols-12 gap-4">
+          {/* Articulo */}
+          <div className="flex items-center gap-2 md:col-span-6">
+            <label className="label">
+              <span className="label-text font-medium mb-2">Articulo</span>
+            </label>
+            <select
+              className="select select-bordered">
+              <option value="none" disabled>Seleccionar articulo</option>
+              {articulos.map((art) => (
+                <option key={art.id} value={art.id}>{art.nombre}</option>
+              ))}
+            </select>
+          </div>
+
+          {/* Observación */}
+          <div className="flex items-center gap-2 md:col-span-6">
+            <label className="label">
+              <span className="label-text font-medium mb-2">Observación</span>
+            </label>
+            <input
+              type="text"
+              className="input input-bordered w-half"
+              name="numero"
+            />
+          </div>
+
+          {/* Cantidad */}
+          <div className="flex items-center gap-2 md:col-span-6">
+            <label className="label">
+              <span className="label-text font-medium mb-2">Cantidad</span>
+            </label>
+            <input
+              type="number"
+              min={0}
+              className="input input-bordered w-half"
+              name="numero"
+              placeholder="0"
+            />
+          </div>
+
+          {/* Precio */}
+          <div className="flex items-center gap-2 md:col-span-6">
+            <label className="label">
+              <span className="label-text font-medium mb-2">Precio</span>
+            </label>
+            <input
+              type="number"
+              min={0}
+              className="input input-bordered w-half"
+              name="numero"
+              placeholder="0"
+            />
+          </div>
+
+        </div>
+    </div>
+  );
 }
 
 export default function ComprProveedorPage() {
@@ -128,14 +198,20 @@ export default function ComprProveedorPage() {
           marginBottom: "1rem",
         }}>Cargar comprobante de proveedor</h1>
 
-        <div id="header" className="mb-4 gap-3">
-          {FormHeader()}
-        </div>
+        <form className="space-y- min-w-[32rem]">
+          <div id="header" className="mb-4 gap-3">
+            {FormHeader()}
+          </div>
+   
+          <div id="main-input" className="mb-4 gap-3">
+            {FormInput()}
+          </div>
 
         <ProvComprTable
           elems={things}
           />
 
+        </form>
       </main>
     </div>
   )
